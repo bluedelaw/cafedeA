567a5ff65fd788f724684e78e69d0bd33b727f06
bluedelaw <daniel.law1128@gmail.com>
2026-08-13 17:28:47 -0700

    fixed analytics message

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,8 @@ import type { Metadata, Viewport } from "next"
 import { Inter } from "next/font/google"
 import "./globals.css"
 
+import { Analytics } from "@vercel/analytics/react"
+
 import Header from "@/components/Header"
 import Footer from "@/components/Footer"
 import DevTools from "@/components/DevTools"
